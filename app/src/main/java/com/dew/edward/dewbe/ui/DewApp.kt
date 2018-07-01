5299677fdb0a40ddb176c3bf8228fd6a0f032255
Edward <ed828a@gmail.com>
2018-07-01 12:35:54 +0930

    Add Enable Multidex

--- app/src/main/java/com/dew/edward/dewbe/ui/DewApp.kt
+++ app/src/main/java/com/dew/edward/dewbe/ui/DewApp.kt
@@ -1,6 +1,6 @@
 package com.dew.edward.dewbe.ui
 
-import android.app.Application
+import android.support.multidex.MultiDexApplication
 import android.support.v4.content.LocalBroadcastManager
 import com.google.android.youtube.player.YouTubePlayer
 
@@ -8,7 +8,7 @@ import com.google.android.youtube.player.YouTubePlayer
 /**
  * Created by Edward on 6/26/2018.
  */
-class DewApp: Application() {
+class DewApp: MultiDexApplication() {
     companion object {
         lateinit var localBroadcastManager: LocalBroadcastManager
         var mYoutubePlayer: YouTubePlayer? = null
