bcc370f133b5657e691ac926cae0c8a82815b02e
Edward <ed828a@gmail.com>
2018-07-07 21:49:24 +0930

    update API Key

--- app/src/main/java/com/dew/edward/dewbe/util/Constants.kt
+++ app/src/main/java/com/dew/edward/dewbe/util/Constants.kt
@@ -1,5 +1,7 @@
 package com.dew.edward.dewbe.util
 
+import com.dew.edward.dewbe.BuildConfig
+
 
 /**
  * Created by Edward on 6/26/2018.
@@ -8,7 +10,7 @@ package com.dew.edward.dewbe.util
 const val YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/"
 const val NETWORK_PAGE_SIZE = 50  //in test, this can be less than 50. it should be 50 in other case
 const val DATABASE_PAGE_SIZE = 20  // should be 20
-const val API_KEY = "AIzaSyA7cdJ8OPftCtkqIBpVuIX5CVtY7BW02JU"
+const val API_KEY = BuildConfig.API_KEY
 const val KEY_QUERY = "query"
 const val DEFAULT_QUERY = "trump"
 const val VIDEO_MODEL = "video_model"
@@ -18,7 +20,6 @@ const val VIDEO_URL = "video_url"
 const val KEY_IS_DB_ENABLED = "is_db_enabled"
 const val KEY_DB_TYPE = "db_type"
 const val KEY_PLAYER_TYPE = "player_type"
-const val DOWNLOAD_BASE_URL = "https://r3---sn-ppoxu-5qaz.googlevideo.com/"
 const val PERMISSION_REQUEST = 88
 
 
